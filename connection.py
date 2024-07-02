766d007d2c0b35dd8848e4fe3b745a49ad9fb1ba
Cesario_Neto <cesarioneto7@gmail.com>
2024-07-01 23:30:15 -0300

    mapping classes and commands

--- connection.py
+++ connection.py
@@ -1,8 +1,8 @@
 import os
 
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from sqlalchemy import text
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
 
@@ -12,16 +12,50 @@ POSTGRES_DB = os.getenv('POSTGRES_DB')
 POSTGRES_HOST = os.getenv('POSTGRES_HOST')
 POSTGRES_PORT = os.getenv('POSTGRES_PORT')
 
-engine = create_engine(f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}')
-conn = engine.connect()
-try:
-    response = conn.execute(text('select * from films;'))
-    for row in response:
-        for col in row:
-            print(col)
-        print(row)
+# Settings
+engine = create_engine(f'postgresql+psycopg2://{POSTGRES_USER}:{
+                       POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}')
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
-except Exception as e:
-    print(e)
-finally:
-    conn.close()
+
+# Entities
+class Films(Base):
+    __tablename__ = 'films'
+
+    title = Column(String, primary_key=True)
+    gender = Column(String, nullable=False)
+    year = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return self.title
+
+# SQL
+
+
+# Delete
+try:
+    session.query(Films).filter(Films.title == 'Um filme').delete()
+    session.commit()
+except:
+    ...
+
+# Insert
+data_insert = Films(title='Um filme', gender='Aventura', year='2005')
+session.add(data_insert)
+session.commit()
+
+# Update
+session.query(Films).filter(
+    Films.title == 'Um filme').update({'gender': 'Drama'})
+session.commit()
+
+# Select
+data = session.query(Films).all()
+print(data)
+for film in data:
+    print(f'{film.title} - {film.gender} - {film.year}')
+
+
+session.close()
